71989bd7e1a751c403aa4ee0d642e04d0728ecb8
nightcbis <65497898+nightcbis@users.noreply.github.com>
2026-07-17 16:48:52 +0200

    Update home.nix
    La till Thunderbird

--- home.nix
+++ home.nix
@@ -80,6 +80,7 @@
     pkgs.fastfetch
     pkgs.discord
     pkgs.dunst
+    pkgs.thunderbird
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
