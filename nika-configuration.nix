1d4d1c895edca53d5e5320e035e07e66e7a2b7a4
nightcbis <andy@cbis.cc>
2026-07-18 12:46:58 +0200

    Changes to be committed: 	new file:   nika-configuration.nix

---
+++ nika-configuration.nix
@@ -0,0 +1,32 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan. And the shared configuration.
+      ./luna-hardware-configuration.nix
+      ./configuration.nix
+    ];
+
+
+  networking.hostName = "Luna"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  #Steam
+  #programs.steam.enable = true;
+
+
+  environment.shellAliases = {
+    nix-rebuild = "sudo nixos-rebuild switch --flake ~andy/NixOs#Nika";
+    nix-testbuild = "nixos-rebuild build --flake ~andy/NixOs#Nika";
+  };
+
+}
