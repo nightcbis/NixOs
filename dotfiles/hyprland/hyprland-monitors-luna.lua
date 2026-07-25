d937853b3f3fe2b9beba4fd2e155e5a06e288830
nightcbis <andy@cbis.cc>
2026-07-25 08:22:22 +0200

    Ta bort scaling. Verkar bugga

--- dotfiles/hyprland/hyprland-monitors-luna.lua
+++ dotfiles/hyprland/hyprland-monitors-luna.lua
@@ -27,14 +27,14 @@ hl.monitor({
   output = "DP-1",
   mode = "preferred",
   position = "0x0",
-  scale = "1",
+  scale = "auto",
 })
 
 hl.monitor({
   output = "DP-2",
   mode = "preferred",
   position = "2560x-350",
-  scale = "1",
+  scale = "auto",
   transform = 3,
 })
 
@@ -42,5 +42,5 @@ hl.monitor({
   output = "HDMI-A-1",
   mode = "preferred",
   position = "-1920x0",
-  scale = "1",
+  scale = "auto",
 })
